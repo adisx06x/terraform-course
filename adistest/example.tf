0981694a37a4dc7507150e11fcce4104a057ce3a
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-04-21 16:45:28 +0000

    adding adistest used for examples on my own

---
+++ adistest/example.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key  = "AKIAIHBXHSUGNE2MFI6Q"
+  secret_key  = "Mbn2itjUTnhg6eQn/M7Gg3aw2fd27JufuBKmMTAd"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+ instance = "${aws_instance.example.id}"
+}
